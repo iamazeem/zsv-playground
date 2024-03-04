e23f4dfa2d55b5f6dd0ec75d791014fbea272733
Azeem Sajid <azeem.sajid@gmail.com>
2024-03-04 17:18:42 +0500

    Initial commit

---
+++ github.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/google/go-github/v60/github"
+)
+
+const (
+	owner    = "liquidaty"
+	repo     = "zsv"
+	cacheDir = "zsv"
+)
+
+func setupCache() bool {
+	fmt.Println("setting up cache")
+
+	fmt.Printf("checking existing cache directory [%v]\n", cacheDir)
+	if stat, err := os.Stat(cacheDir); err == nil && stat.IsDir() {
+		fmt.Println("cache directory exists")
+		return true
+	}
+
+	fmt.Println("cache directory not found, creating...")
+	if err := os.MkdirAll(cacheDir, os.ModePerm); err != nil {
+		fmt.Printf("failed to create cache directory [%v]\n", err)
+		return false
+	}
+
+	fmt.Println("cache directory set up successfully")
+	return true
+}
+
+func loadCache() (map[string]int64, bool) {
+	fmt.Println("loading cache")
+
+	c := map[string]int64{}
+
+	entries, err := os.ReadDir(cacheDir)
+	if err != nil {
+		fmt.Printf("error while reading cache directory, %v\n", err)
+		return nil, false
+	}
+
+	for _, e := range entries {
+		name := e.Name()
+		info, err := e.Info()
+		if err != nil {
+			fmt.Printf("failed to get directory entry info [%v], %v\n", name, err)
+			return nil, false
+		}
+		c[name] = info.Size()
+	}
+
+	fmt.Println("cache loaded successfully")
+	return c, true
+}
+
+func downloadLatestRelease() error {
+	if !setupCache() {
+		return fmt.Errorf("failed to set up cache")
+	}
+
+	c, ok := loadCache()
+	if !ok {
+		return fmt.Errorf("failed to load cache")
+	}
+
+	// fmt.Println(c)
+	// fmt.Println("listing releases...")
+
+	ctx := context.Background()
+	client := github.NewClient(nil)
+
+	opts := &github.ListOptions{Page: 1, PerPage: 3}
+	releases, _, err := client.Repositories.ListReleases(ctx, owner, repo, opts)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	// TODO: Purge previous cached releases in case of new ones
+	// fmt.Println(len(releases))
+
+	// tag > id
+	m := map[string]int64{}
+
+	for _, r := range releases {
+		tag := r.GetTagName()
+		for _, a := range r.Assets {
+			if strings.HasSuffix(*a.Name, "amd64-linux-gcc.tar.gz") {
+				id := a.GetID()
+				size := a.GetSize()
+				fmt.Printf("checking %v [size: %v]\n", tag, size)
+				if cachedSize, ok := c[tag + ".tar.gz"]; ok && int64(size) == cachedSize {
+					fmt.Printf("%v found in cache, skipped\n", tag)
+				} else {
+					m[tag] = id
+				}
+			}
+		}
+	}
+
+	// fmt.Println(m)
+	downloads := 0
+	for tag, id := range m {
+		fmt.Printf("downloading %v [id: %v]\n", tag, id)
+		rc, _, err := client.Repositories.DownloadReleaseAsset(ctx, owner, repo, id, http.DefaultClient)
+		if err != nil {
+			fmt.Printf("failed to download %v\n", tag)
+			return err
+		}
+
+		filename := fmt.Sprintf("zsv/%v.tar.gz", tag)
+		file, _ := os.Create(filename)
+		io.Copy(file, rc)
+
+		downloads++
+
+		file.Close()
+		rc.Close()
+	}
+
+	fmt.Printf("cache populated successfully [downloads: %v]\n", downloads)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module zsv-playground
 
 go 1.21.4
+
+require github.com/google/go-github/v60 v60.0.0
+
+require github.com/google/go-querystring v1.1.0 // indirect
